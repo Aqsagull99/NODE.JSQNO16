685a645b1d361bda3814cdb96328d6a618d02b37
Aqsagull99 <159878357+Aqsagull99@users.noreply.github.com>
2024-02-17 09:12:08 +0500

    Add files via upload

---
+++ qno16.js
@@ -0,0 +1,25 @@
+// QNO:16
+// More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests to invite to dinner.
+// Start with your program from Exercise 15. Add a print statement to the end of your program informing people that you found a bigger dinner table.
+// • Add one new guest to the beginning of your array.
+// • Add one new guest to the middle of your array. • Use append() to add one new guest to the end of your list. • Print a new set of invitation messages, one for each person in your list.
+// let guestlist=["saba","noreen","naila","musfira","mehru", "muntaha"]
+//  let canNotAttend:string = "noreen"
+//  let newGuest:string ="mahru"
+//  guestlist[guestlist.indexOf(canNotAttend)] =newGuest;
+//  console.log(guestlist)
+//  guestlist.map((item)=>
+//  console.log(`dear ${item}, i found a bigger dinner table`))
+// let guestbegin="sana"
+// guestlist.unshift(guestbegin)
+// console.log(guestlist)
+// guestlist.map((item)=>
+//  console.log(`dear ${item}, i found a bigger dinner table`))
+// let middleguest="fatima";
+// let middleindex= guestlist.length/2
+// guestlist.splice(middleindex,0,middleguest)
+// console.log(guestlist)
+// guestlist.push("saima")
+// console.log(guestlist)
+// guestlist.map((item)=>
+//  console.log(`dear ${item}, you are invited for birthday dinner party`))
